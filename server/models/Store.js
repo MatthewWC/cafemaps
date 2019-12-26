daa6c5fee9239a5985cab9103624705a31e621f1
Matthew Cook <matthew.w.cook94@gmail.com>
2019-12-26 07:44:01 -0500

    new store model

--- server/models/Store.js
+++ server/models/Store.js
@@ -1,6 +1,66 @@
+const { uuid }= require('uuidv4')
+
+// apple pay
+// samsung pay
+
 module.exports = (sequelize, DataTypes) => {
   const Store = sequelize.define('Store', {
-    storeName: DataTypes.STRING
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: () => uuid()
+    },
+    storeName: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    addressOne: {
+      type: DataTypes.STRING
+    },
+    addressTwo: {
+      type: DataTypes.STRING
+    },
+    city: {
+      type: DataTypes.STRING
+    },
+    state: {
+      type: DataTypes.STRING
+    },
+    zipcode: {
+      type: DataTypes.STRING(5)
+    },
+    rating: {
+      type: DataTypes.FLOAT
+    },
+    wifi: {
+      type: DataTypes.BOOLEAN
+    },
+    bakery: {
+      type: DataTypes.BOOLEAN
+    },
+    milkAlt: {
+      type: DataTypes.BOOLEAN
+    },
+    indoorSeating: {
+      type: DataTypes.BOOLEAN
+    },
+    driveThru: {
+      type: DataTypes.BOOLEAN
+    },
+    clubCard: {
+      type: DataTypes.BOOLEAN
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    }
   })
 
   Store.order = 0
