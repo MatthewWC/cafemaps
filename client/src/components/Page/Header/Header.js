3d07c0911f9682189846008288f8f9958db6134c
Matthew Cook <matthew.w.cook94@gmail.com>
2020-01-02 06:56:08 -0500

    added menu

---
+++ client/src/components/Page/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserMenu from './UserMenu'
+// ---- material-ui imports ----
+// -----------------------------
+
+function Header () {
+  return (
+    <div>
+      <UserMenu/>
+      <h1>header</h1>
+    </div>
+  )
+}
+
+export default Header
