a2cb3999c77269fca65939807453647200136a97
Matthew Cook <matthew.w.cook94@gmail.com>
2020-01-02 06:52:54 -0500

    added header

--- client/src/components/Page/Page.js
+++ client/src/components/Page/Page.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
+import Header from './Header'
 
 function Page ({ children, ...otherProps}) {
 
   return (
     <Fragment>
+      <Header/>
       <div>
         {children}
       </div>
