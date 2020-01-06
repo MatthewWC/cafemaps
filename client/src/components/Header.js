c4b181c0840f053a51643237c5cdc3f69bccef30
Matthew Cook <matthew.w.cook94@gmail.com>
2020-01-05 20:50:48 -0500

    fixed render issues

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,16 +5,17 @@ import IconButton from '@material-ui/core/IconButton'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 // -----------------------------
 
 const useStyles = makeStyles(theme => ({
   root: {
-    position: 'relative',
-    display: 'flex'
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'brown',
+    padding: '10px'
   },
   menu: {
 
@@ -43,45 +44,41 @@ function Header () {
 
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography variant='h6' className={classes.title}>
-            Coffee Maps
-          </Typography>
-          <IconButton
-            className={classes.menuButton}
-            onClick={handleClick}>
-            <AccountCircleIcon fontSize='large'/>
-          </IconButton>
-          <Menu
-            className={classes.menu}
-            id='simple-menu'
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            {localStorage.getItem('token') ? 
-              (<MenuItem 
-                onClick={() => {
-                  localStorage.removeItem('token')
-                  handleClose()
-                }}
-                component={Link}
-                to='/login'
-              >Logout</MenuItem>) : 
-              (<MenuItem 
-                onClick={() => {
-                  handleClose()
-                }}
-                component={Link}
-                to='/login'
-              >Login</MenuItem>)
-            }
-          </Menu>
-        </Toolbar>
-      </AppBar>
+      <Typography variant='h6' className={classes.title}>
+        Coffee Maps
+      </Typography>
+      <IconButton
+        className={classes.menuButton}
+        onClick={handleClick}>
+        <AccountCircleIcon fontSize='large'/>
+      </IconButton>
+      <Menu
+        className={classes.menu}
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        {localStorage.getItem('token') ? 
+          (<MenuItem 
+            onClick={() => {
+              localStorage.removeItem('token')
+              handleClose()
+            }}
+            component={Link}
+            to='/login'
+          >Logout</MenuItem>) : 
+          (<MenuItem 
+            onClick={() => {
+              handleClose()
+            }}
+            component={Link}
+            to='/login'
+          >Login</MenuItem>)
+        }
+      </Menu>
     </div>
   )
 }
