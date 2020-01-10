fda974ba7159011b21e89ec0b37d0e321ba9f9d5
Matthew Cook <matthew.w.cook94@gmail.com>
2020-01-10 15:46:17 -0500

    added randys beautiful code

---
+++ server/middleware/auth-middleware.js
@@ -0,0 +1,55 @@
+const { verifyBearerToken } = require('./util')
+const { models } = require('../config/sequelize')
+
+function accessBuilder(role) {
+  return async (req, res, next) => {
+
+    // if public resolver, skip auth
+    if(role === 'public') return next()
+
+    // get auth header
+    const authHeader = req.header('Authorization')
+    
+    // if header exists split, link for better understanding.
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split 
+    const bearerHeader = authHeader ? authHeader.split('Bearer ')[1] : undefined
+
+    // req.query.bearer <== restful api stuff, review later
+    const bearer = req.query.bearer || bearerHeader
+
+    if(bearer){
+      try{
+        // await promise to verify users token, and save email.
+        const { email } = await verifyBearerToken(bearer, role)
+        // access data model and get user associated with email
+        const account = await models.User.findOne({
+          where: {
+            email: email
+          }
+        })
+      } catch (error){
+        // link up error handler
+      }
+    }
+  }
+}
+
+// auth middleware for rest router, not using atm.
+function authMiddleware(router){
+  console.log(router)
+}
+
+// accessed through server context, when auth is called executes function
+authMiddleware.graphql = (req, res) => {
+  // what allows auth to accept a role, creates promise
+  return role => new Promise((resolve, reject) => {
+    // handle role, and token checks
+    accessBuilder(role)(req, res, err => {
+      if (err) return reject(err)
+      return resolve()
+    })
+  })
+}
+
+
+module.exports = authMiddleware
