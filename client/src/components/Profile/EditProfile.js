045462d7cd51f47340b3522a42080e7e198b9e16
Matthew Cook <matthew.w.cook94@gmail.com>
2020-01-17 09:19:52 -0500

    new component

--- client/src/components/Profile/EditProfile.js
+++ client/src/components/Profile/EditProfile.js
@@ -10,9 +10,10 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import { Typography } from '@material-ui/core'
 import Link from '@material-ui/core/Link'
-import Input from '@material-ui/core/Input'
 import Divider from '@material-ui/core/Divider'
 // -----------------------------
+
+//TODO: handle DDOS on register request
 
 // material-ui styles
 const useStyles = makeStyles(theme => ({
@@ -39,6 +40,21 @@ const useStyles = makeStyles(theme => ({
   },
   fileInput: {
     display: 'none'
+  },
+  image: {
+    maxHeight: '250px',
+  },
+  imageText: {
+    margin: theme.spacing(1),
+    textTransform: 'capitalize',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '40%'
+  },
+  imageContainer: {
+    position: 'relative',
+    width: '100%'
   }
 }))
 
@@ -49,35 +65,61 @@ function EditProfile (props) {
   // element references
   let editProfileForm = React.createRef()
   let successfulText = React.createRef()
+  let imageFile = React.createRef()
+  let imageEle = React.createRef()
 
   const { loading, error, data } = useQuery(GET_USER, { variables: { email: localStorage.getItem('email') }})
 
   async function handleSubmit(event, props){
+    let imageUrl
+
     event.preventDefault()
 
-    axios.post("http://localhost:4000/upload")
     try{
-      await validate(editProfileForm)
-      const results = await props.client.mutate({
+      //validate form
+      validate(editProfileForm)
+      // if image change, upload image
+      if(imageFile.current.files[0]){
+        const data = new FormData()
+        let config = {
+          headers: { 'Content-Type': 'multipart/form-data'}
+        }
+        data.append('file', imageFile.current.files[0])
+        imageUrl = await axios.post('http://localhost:4000/upload', data, config)
+        imageUrl = imageUrl.data
+      }
+      // hit database
+      await props.client.mutate({
         mutation: gql`
-          mutation updateUser($firstName: String, $lastName: String){
-            updateUser(firstName: $firstName, lastName: $lastName){
+          mutation updateUser($imageUrl: String, $firstName: String, $lastName: String){
+            updateUser(imageUrl: $imageUrl, firstName: $firstName, lastName: $lastName){
               token 
               user{
+                imageUrl
                 firstName 
                 lastName
               }
             }
           }`,
         variables: {
+          imageUrl: imageUrl || imageEle.current.src,
           firstName: editProfileForm.current.firstName.value,
           lastName: editProfileForm.current.lastName.value
         }
       })
+      // if successful, show it
       successfulText.current.hidden = false
-      console.log(results.data.updateUser )
     } catch (error) {
-      console.log(error)
+      throw new Error('Something bad happend. Contact support.')
+    }
+  }
+
+  // handles file display when uploaded
+  function handleChange(event){
+    // if file selected changes
+    if(event.target.files[0]){
+      let image = URL.createObjectURL(event.target.files[0])
+      imageEle.current.src = image
     }
   }
 
@@ -97,28 +139,43 @@ function EditProfile (props) {
       </div>
     )
   }
-
+  
   return(
     <div className={classes.root}>
       <Typography 
         className={classes.successfulText}
         ref={successfulText} 
-        hidden={true}>
-        Update Successful
-        <Link
-          href={'./profile'}>
-          view profile!
-        </Link>
+        hidden={true}
+      >
+        Update Successful{``}  <Link href={'./profile'}>view profile!</Link>
       </Typography>
       <div className={classes.editProfile}>
-          <div>
+          <div className={classes.imageContainer}>
             <label display='inline-block'>
-              <img src={data.getUser.imageUrl ||'profile_basic.png'} alt='profile' height='auto' width='100%'/>
-              <input className={classes.fileInput} type='file'/>
+              <img 
+                ref={imageEle}
+                src={data.getUser.imageUrl || 'profile_basic.png'} 
+                alt='profile' 
+                className={classes.image}
+                height='auto'
+                width='100%'/>
+              <input 
+                ref={imageFile}
+                className={classes.fileInput} 
+                type='file'
+                accept="image/*"
+                onChange={(event) => {
+                  handleChange(event)
+                }}/>
+              <Button 
+                component='p'
+                variant='contained'
+                className={classes.imageText}>Edit Avatar</Button>
             </label>
           </div>
           <Divider/>
         <form
+          name='updateUserForm'
           ref={editProfileForm}
           onSubmit={(event) => {
             handleSubmit(event, props)
@@ -162,6 +219,7 @@ export default EditProfile
 const GET_USER = gql`
   query getUser($email: String!){
     getUser(email: $email){
+      imageUrl
       firstName 
       lastName
       email
