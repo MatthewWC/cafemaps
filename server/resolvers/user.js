d85551696736bb7b3e5956ce62266e186ed382e1
Matthew Cook <matthew.w.cook94@gmail.com>
2020-01-17 09:29:09 -0500

    resolvers overhaul

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,5 +1,5 @@
 const { hashPassword, comparePassword, createBearerToken, verifyBearerToken } = require('../middleware/util')
-const { UnknownError, ForbiddenError, UnauthorizedError } = require('../errors')
+const { ForbiddenError, UnauthorizedError } = require('../errors')
 
 //TODO: on account delete, send email
 //TODO: email verify
@@ -7,41 +7,30 @@ const { UnknownError, ForbiddenError, UnauthorizedError } = require('../errors')
 module.exports = {
   Query: {
     getUser: async (_, args, context, info) => {
-      try {
-        // handle auth
-        await context.auth('USER')
-        // return user associated with token
-        return context.req.user
-      } catch (error) {
-        throw new UnknownError
-      }
+      // handle auth
+      await context.auth('USER')
+      // return user associated with token
+      return context.req.user
     },
     getUsers: async (_, args, context, info) => {
-      try{
-        // handle auth
-        await context.auth('ADMIN')
-        // return array of users matching search
-        return await context.models.User.findAll({
-          where: {
-            firstName: args.firstName
-          }
-        })
-      } catch (error) {
-         throw new UnknownError
-      }
+      // handle auth
+      await context.auth('ADMIN')
+      // return array of users matching search
+      return await context.models.User.findAll({
+        where: { firstName: args.firstName }
+      })
     }
   },
   Mutation: {
     register: async (_, args, context, info) => {
       try{
-        context.auth('public')
+        // handle auth
+        context.auth('PUBLIC')
         // get hash
         let hash = await hashPassword(args.password)
         // create user
         return await context.models.User.create({
           role: 'USER',
-          firstName: args.firstName,
-          lastName: args.lastName,
           passwordHash: hash,
           email: args.email
         })
@@ -56,7 +45,7 @@ module.exports = {
       let user
       try{
         // handle auth
-        context.auth('public')
+        context.auth('PUBLIC')
         // get user
         context.req.user = await context.models.User.findOne({ where: { email: email }})
         user = context.req.user
@@ -76,21 +65,19 @@ module.exports = {
       return { token, user }
     },
     updateUser: async (_, args, context, info) => {
-      // TODO: create new token after update
-      let newPassword
-      let token
-      let user
-      try{
-        // check login
+        let token
+        let newPassword
+        // handle auth
         await context.auth('USER')
         // get user
-        user = context.req.user
+        const user = context.req.user
         // hash new password
         if(args.password){
           newPassword = await hashPassword(args.password)
         }
         // update user
         await context.req.user.update({
+          imageUrl: args.imageUrl || context.req.user.dataValues.imageUrl,
           firstName: args.firstName || context.req.user.dataValues.firstName,
           lastName: args.lastName || context.req.user.dataValues.lastName,
           email: args.email || context.req.user.dataValues.email,
@@ -104,23 +91,21 @@ module.exports = {
             email: context.req.user.dataValues.email
           }, context.req.user.role)
         }
-      } catch (error) {
-        throw new UnknownError
-      }
       return { token, user }
     },
     deleteUser: async (_, args, context, info) => {
       let user
       try{
-        // get user
+        // handle auth
         await context.auth('USER')
+        // get reference to user
         user = context.req.user
         //check password
         await comparePassword(args.password, user.dataValues.passwordHash)
         // delete user
         await context.req.user.destroy({ force: true })
       } catch (error) {
-        throw new UnknownError
+        throw error
       }
       return user
     }
