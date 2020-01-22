8949aecafb7e8c477e1065afefc62749a725b028
Matthew Cook <matthew.w.cook94@gmail.com>
2020-01-22 14:59:08 -0500

    store model

---
+++ client/src/components/Store/storeFields.js
@@ -0,0 +1,26 @@
+const storeFields = {
+  companyName: '',
+  storeName: '', 
+  email: '',
+  imageUrl: '',
+  addressOne: '',
+  addressTwo: '',
+  city: '',
+  state: '',
+  zipcode: '', 
+  moHours: '',
+  tuHours: '',
+  weHours: '',
+  thHours: '',
+  frHours: '',
+  saHours: '',
+  suHours: '',
+  rating: '', 
+  wifi: false, 
+  bakery: false,
+  milkAlt: false, 
+  indoorSeating: false,
+  driveThru: false
+}
+
+export default storeFields
