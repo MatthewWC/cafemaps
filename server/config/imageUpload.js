2706f9f1ec803fca43617dd54011486e839549ba
Matthew Cook <matthew.w.cook94@gmail.com>
2020-02-27 09:56:03 -0500

    added try catch

--- server/config/imageUpload.js
+++ server/config/imageUpload.js
@@ -1,24 +1,28 @@
 const { s3 } = require('./s3')
 const fs = require('fs')
+const { UnknownError } = require('../errors')
 
 exports.imageUpload = async function(req, res) {
-  console.log(req.file)
-  
-  // build request
-  const s3Params = {
-    Bucket: 'amplifys3storageimages-default',
-    Key: req.file.originalname,
-    ContentEncoding: 'base64',
-    ContentType: 'image/jpeg',
-    ACL: 'public-read',
-    Body: req.file.buffer
-  }
-    
-  //send request to s3
-  s3.upload(s3Params, function(error, data){
-    if(error){
-      throw error
+ try{
+    // build request  
+    const s3Params = {
+      Bucket: 'amplifys3storageimages-default',
+      Key: req.file.originalname,
+      ContentEncoding: 'base64',
+      ContentType: 'image/jpeg',
+      ACL: 'public-read',
+      Body: req.file.buffer
     }
-    res.end(data.Location)
-  }) 
+      
+    //send request to s3
+    s3.upload(s3Params, function(error, data){
+      if(error){
+        return error
+      }
+      res.end(data.Location)
+    }) 
+  }
+  catch(error){
+    throw new UnknownError
+  }
 }
