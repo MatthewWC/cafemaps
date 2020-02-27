405eea5144e1bac6f146ca6b693b7dd899bce5c0
Matthew Cook <matthew.w.cook94@gmail.com>
2020-02-27 09:11:14 -0500

    login pushed through FormBuilder

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,175 +1,37 @@
 import React from 'react'
-
 import gql from 'graphql-tag'
 
-import validate from '../tools/validator'
+import Grid from '@material-ui/core/Grid'
 
-// ---- M-UI imports ----
-import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
-import FormHelperText from '@material-ui/core/FormHelperText'
-import Typography from '@material-ui/core/Typography'
-import Link from "@material-ui/core/Link"
-import Divider from '@material-ui/core/Divider'
-// ---- M-UI imports ----
+import FormBuilder from '../tools/FormBuilder'
 
-//TODO: add google sigin in
-//TODO: forgot password page & link
-//TODO: fix graphql error handling on query/mutation return
+//TODO: add register link
 
-// M-UI styles
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    height: '90vh',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    maxWidth: '1024px',
-    padding: '5%'
-  },
-  login: {
-    textAlign: 'center',
-    margin: 'auto',
-    width: '100%',
-    maxWidth: '300px'
-  },
-  title: {
-
-  },
-  button: {
-    width: '100%'
-  },
-  fields : {
-    width: '100%'
-  },
-  errors: {
-    margin: theme.spacing(2)
-  },
-  divider: {
-    marginTop: theme.spacing(5)
-  },
-  registerText: {
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '10px',
-    
-  }
-}))
-
-function Login (props){
-  // M-UI styles instance
-  const classes = useStyles()
-
-  // refs
-  let loginForm = React.createRef()
-  let errorText = React.createRef()
-
-  // when button clicked
-  async function handleSubmit(event, props){
-    event.preventDefault()
-
-    // check for client errors & run mutation if none
-    try{
-      validate(loginForm)
-      const results = await props.client
-        .mutate({
-          mutation: gql`
-            mutation login($email: String!, $password: String!){
-              login(email: $email, password: $password){
-                user{
-                  email
-                  role
-                }
-                token
-              }
-            }`,
-          variables: {
-            email: loginForm.current.email.value,
-            password: loginForm.current.password.value
-          }
-        })
-    // successful response
-    localStorage.setItem('token', results.data.login.token)
-    localStorage.setItem('email', results.data.login.user.email)
-    localStorage.setItem('role', results.data.login.user.role)
-    props.history.push('/')
-    } 
-    catch(err){
-      console.log(err)
-      let errMsg
-      // handle unsuccessful response
-      errMsg = err.toString().lastIndexOf(':') + 1
-      errorText.current.innerHTML = err.toString().substring(errMsg, 75)
-    }
-  }
+function Login (props) {
 
   return(
-    <div className={classes.root}>
-      <div className={classes.login}>
-      <form        
-        name='loginForm'  
-        ref={loginForm}
-        onSubmit={(event) => {
-          handleSubmit(event, props)
-        }}>
-        <Typography
-          variant='h4'>
-          Log in
-        </Typography>
-        <Typography
-          variant='h6'>
-          &
-        </Typography>
-        <Typography
-          variant='h6'>
-          Start Mapping!
-        </Typography>
-        <TextField
-          className={classes.fields}
-          name='email'
-          margin='normal'
-          variant='outlined'
-          label='Email'
-          InputLabelProps={{
-            shrink: true,
-          }}
-          type='text'/>
-        <TextField
-          className={classes.fields}
-          name='password'
-          margin='normal'
-          variant='outlined'
-          label='Password'
-          InputLabelProps={{
-            shrink: true,
-          }}
-          type='text'/>
-        <FormHelperText
-          className={classes.errors}
-          ref={errorText}
-          error={true}>
-        </FormHelperText>
-        <Button
-          className={classes.button}
-          variant='contained'
-          type='submit'>
-          LOGIN
-        </Button>
-      </form>
-      <Divider className={classes.divider}/>
-        <div className={classes.registerText}>
-          <Typography> 
-            New around here?...
-            <Link
-              href={'/register'}>
-              Register.
-            </Link>
-          </Typography>
-        </div>
-      </div>
-    </div>
+    <Grid container justify='center'>
+      <FormBuilder
+        formName={'Login Form'}
+        fields={{
+          email: 'String',
+          password: 'String'
+        }}
+        mutation={LOGIN}
+        props={props}/>
+    </Grid>
   )
 }
 
 export default Login
+
+const LOGIN = gql`
+  mutation login($email: String!, $password: String!){
+    login(email: $email, password: $password){
+      token,
+      user{
+        email
+        role
+      }
+    }
+  }`
