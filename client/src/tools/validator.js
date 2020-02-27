36c8067486047644c5b19b3c7b0619aee888e0b2
Matthew Cook <matthew.w.cook94@gmail.com>
2020-02-27 09:52:53 -0500

    added switch cases

--- client/src/tools/validator.js
+++ client/src/tools/validator.js
@@ -1,47 +1,69 @@
 import validator from 'validator'
 
 const validate = (form) => {
-  let formFields = form.current
+  let formFields = form
+
   switch(formFields.name){
-    case 'updateUserForm':
+    case 'Update Profile Form':
       // check for random character if field isnt empty
       Object.keys(formFields).forEach(function (item){
-        if(formFields[item].type === 'text' && formFields[item].value !== ''){
-          if(validator.isAlphanumeric(formFields[item].value) === false){
-            throw new Error('Fields can only contain letters A-Z & numbers 0-9.')
-          }
-        }
-      })
-      break
-    case 'loginForm':
-      // check for empty fields on email & password
-      if(validator.isEmpty(form.current[0].value) || validator.isEmpty(form.current[2].value)){
-        throw new Error('Fields cannot be empty.')
-      }
-      // check if email is valid
-      if(validator.isEmail(form.current[0].value) === false){
-        throw new Error('Thats not a real email.')
-      }
-      break
-    case 'registerForm':
-      // check for empty fields
-      Object.keys(formFields).forEach(function (item){
+        // check for empty fields
         if(formFields[item].type === 'text'){
           if(validator.isEmpty(formFields[item].value)){
             throw new Error('Fields cannot be empty.')
           }
         }
+        // email check
+        if(formFields[item].name === 'email'){
+          if(validator.isEmail(formFields[item].value) === false){
+            throw new Error('Thats not a real email.')
+          }
+        }
+
+        if(formFields[item].type === 'text' && formFields[item].value !== ''){
+          if(validator.isAlphanumeric(formFields[item].value) === false && formFields[item].name !== 'email'){
+            throw new Error('Fields can only contain letters A-Z & numbers 0-9.')
+          }
+        }
+      })
+      break
+    case 'Login Form':
+      Object.keys(formFields).forEach(function (item){
+        // check for empty fields
+        if(formFields[item].type === 'text'){
+          if(validator.isEmpty(formFields[item].value)){
+            throw new Error('Fields cannot be empty.')
+          }
+        }
+        // email check
+        if(formFields[item].name === 'email'){
+          if(validator.isEmail(formFields[item].value) === false){
+            throw new Error('Thats not a real email.')
+          }
+        }
       })
       // check if email is valid
-      if(validator.isEmail(form.current[0].value) === false){
-        throw new Error('Thats not a real email.')
-      }
-      // compare password & confirm password
-      if(form.current[2].value !== form.current[4].value){
+      break
+    case 'Register Form':
+      Object.keys(formFields).forEach(function (item){
+        // check for empty fields
+        if(formFields[item].type === 'text'){
+          if(validator.isEmpty(formFields[item].value)){
+            throw new Error('Fields cannot be empty.')
+          }
+        }
+        // email check
+        if(formFields[item].name === 'email'){
+          if(validator.isEmail(formFields[item].value) === false){
+            throw new Error('Thats not a real email.')
+          }
+        }
+      })
+      if(formFields['confirmPassword'].value !== formFields['password'].value){
         throw new Error('Passwords do not match.')
       }
       break
-    case 'createStoreForm':
+    case 'Create Store Form':
       // check for empty fields
       Object.keys(formFields).forEach(function (item){
         if(formFields[item].type === 'text'){
@@ -50,6 +72,42 @@ const validate = (form) => {
           }
         }
       })
+      break
+    case 'Create Coffee Form':
+        // check for empty fields
+        Object.keys(formFields).forEach(function (item){
+          if(formFields[item].type === 'text'){
+            if(validator.isEmpty(formFields[item].value)){
+              throw new Error('Fields cannot be empty')
+            }
+          }
+        })
+      break
+    case 'Update Store Form':
+      Object.keys(formFields).forEach(function (item){
+        // check for empty fields
+        if(formFields[item].type === 'text'){
+          if(validator.isEmpty(formFields[item].value)){
+            throw new Error('Fields cannot be empty')
+          }
+        }
+        // email check
+        if(formFields[item].name === 'email'){
+          if(validator.isEmail(formFields[item].value) === false){
+            throw new Error('Thats not a real email.')
+          }
+        }
+      })
+      break
+      case 'Update Coffee Form':
+        // check for empty fields
+        Object.keys(formFields).forEach(function (item){
+          if(formFields[item].type === 'text'){
+            if(validator.isEmpty(formFields[item].value)){
+              throw new Error('Fields cannot be empty')
+            }
+          }
+        })
       break
     default:
       // if user somehow changed form name
